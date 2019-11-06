5588f5cfb801ba62d86e17c824f1c72b633a3459
SeanLeeDev <33349300+SeanLeeDev@users.noreply.github.com>
2019-11-06 16:55:10 -0600

    Add files via upload

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SouthernTreasuresDAL;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (DALContext ctx = new DALContext())
+            {
+                ctx.ConnectionString = @"Data Source=GDC-BC-13\SQLEXPRESS;Initial Catalog=SouthernTreasures;Integrated Security=True";
+                ctx.ReadSpecificOffer(8);
+                //var x = ctx.ReadAllUsers();
+                //foreach (var user in x)
+                //{
+                //   Console.WriteLine(user);
+                //}
+            }
+        }
+
+    }
+}
---
+++ Logger/Class1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace Logger
+{
+    static public class LogMe
+    {
+        // the connection string is only loaded one time, at the start of the application
+        static string connectionstring;
+        // this is a static constructor.  It is used to initialize the static connectionstring    
+        static LogMe()
+        {
+            connectionstring = System.Configuration.ConfigurationManager.ConnectionStrings["DefaultConnectionString"].ConnectionString;
+        }
+        // this method is static so that it will have semantics like Console.WriteLine
+        public static void Log(Exception ex)
+        {
+            try
+            {
+                using (SqlConnection con = new SqlConnection(connectionstring))
+                {
+                    con.Open();
+                    using (var com = con.CreateCommand())
+                    {
+                        com.CommandText = "InsertLogItem";
+                        com.CommandType = System.Data.CommandType.StoredProcedure;
+                        com.Parameters.AddWithValue("@message", ex.Message);
+                        com.Parameters.AddWithValue("@stacktrace", ex.StackTrace.ToString());
+                        com.ExecuteNonQuery();
+                    }
+                }
+            }
+            catch (Exception exc)
+            {
+                var p = HttpContext.Current.Server.MapPath("~");
+                p += @"ErrorLog.Log";
+                System.IO.File.AppendAllText(p,
+"while attempting to record the original exception to the database, this exception occurred\r\n");
+                System.IO.File.AppendAllText(p, exc.ToString());
+                System.IO.File.AppendAllText(p,
+"This is the Original Exception that was attempted to be written to the database\r\n");
+                System.IO.File.AppendAllText(p, ex.ToString());
+
+            }
+        }
+    }
+}
