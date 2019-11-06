fe7e3404dc3670e38f48f948204531048c87ab29
SeanLeeDev <33349300+SeanLeeDev@users.noreply.github.com>
2019-11-06 16:55:56 -0600

    Add files via upload

---
+++ SouthernTreasures/Global.asax.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+using System.Security.Principal;
+
+namespace SouthernTreasures
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+        protected void Application_AcquireRequestState(object sender, EventArgs e)
+        {
+            string UserName = Session["AUTHUserName"] as string;
+            string SessRoles = Session["AUTHRoles"] as string;
+            if (string.IsNullOrEmpty(UserName))
+            {
+                return;
+            }
+            GenericIdentity i = new GenericIdentity(UserName, "MyAuthMeth");
+            if (SessRoles == null) { SessRoles = ""; }
+            string[] roles = SessRoles.Split(' ');
+            GenericPrincipal p = new GenericPrincipal(i, roles);
+            HttpContext.Current.User = p;
+    }
+
+    }
+}
